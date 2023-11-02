d7467fff8933af749ab661fe93ce908e231ef784
namishk <namishkhandelwal1@gmail.com>
2023-11-02 14:21:43 +0530

    feat:openzeplin ERC721

---
+++ contracts/Multility.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: CC0
+pragma solidity ^0.8.9;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Multility is ERC721 {
+    constructor() {}
+}
